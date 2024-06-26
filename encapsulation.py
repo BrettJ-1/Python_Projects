1ab30abbf2e48a0622d6e4123428f49016e99552
BrettJ-1 <167814883+BrettJ-1@users.noreply.github.com>
2024-06-26 16:13:20 -0700

    Commented

--- encapsulation.py
+++ encapsulation.py
@@ -1,14 +1,14 @@
 class Protected:
     def __init__(self):
-        self.__privateVar = 23
+        self.__privateVar = 23 #Declare Variable
         
     def prntPrivate (self):
-        print (self.__privateVar)
+        print (self.__privateVar) #Print Variable
 
-    def declarePrivate (self, private):
+    def declarePrivate (self, private): #Set Variable to private
         self.__privateVar = private
 
 obj = Protected()
 obj.prntPrivate()
-obj.declarePrivate(24)
+obj.declarePrivate(24) #Changed Variable
 obj.prntPrivate()
