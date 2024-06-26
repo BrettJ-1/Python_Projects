bd4ec96274b1cdfab723ea30cad649b7ecb1a20f
BrettJ-1 <167814883+BrettJ-1@users.noreply.github.com>
2024-06-26 15:59:13 -0700

    Update

--- file_extension.py
+++ file_extension.py
@@ -4,34 +4,24 @@ conn = sqlite3.connect('file_extension.db')
 
 with conn:
     cur = conn.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS tbl_extension( \
-        ID INTEGER PRIMARY KEY AUTOINCREMENT, \
-        col_information.docx TEXT, \
-        col_Hello.txt TEXT, \
-        col_myImage.png TEXT \
-        col_myMovie.mpg TEXT \
-        col_World.txt TEXT, \
-        col_data.pdf TEXT \
-        col_myPhoto.jpg TEXT \
-        )")
+    cur.execute("CREATE TABLE IF NOT EXISTS tbl_extension( \ 
+        ID INTEGER PRIMARY KEY AUTOINCREMENT, \                   #Created datbase with integer and string
+        col_filename TEXT,)")
     conn.commit()
 conn.close()
 
-
 conn = sqlite3.connect('file_extension.db')
 
 with conn:
     cur = conn.cursor()
-    cur.execute("INSERT INTO tbl_extension(information.docx, Hello.txt, myImage.png, myMovie.mpg, World.txt, data.pdf, myPhoto.jpg) VALUES (?, ?, ?, ?, ?, ?, ?)", \
-                ('Classified', 'Hello!', 'Cat Photo', 'Back to The Future', 'Earth', 'Company Files', 'Smiling'))
-    
+    cur.execute("INSERT INTO tbl_extension(information.docx, Hello.txt, myImage.png, myMovie.mpg, World.txt, data.pdf, myPhoto.jpg) VALUES (?, ?, ?, ?, ?, ?, ?)"  #Inserted values into tbl_extension
 conn.close()
 
 conn = sqlite3.connect('file_extension.db')
 
 with conn:
     cur = conn.cursor()
-    cur.execute("SELECT information.docx, Hello.txt, myImage.png, myMovie.mpg, World.txt, data.pdf, myPhoto.jpg FROM tbl_extension WHERE TEXT = '.txt'")
+    cur.execute("SELECT information.docx, Hello.txt, myImage.png, myMovie.mpg, World.txt, data.pdf, myPhoto.jpg FROM tbl_extension WHERE TEXT = '.txt'") #Selecting data with the filename extension of .txt
     varPerson = cur.fetchall()
     for item in varPerson:
         msg = "First: ()\nSecond: ()".format(item[0],item[1])
