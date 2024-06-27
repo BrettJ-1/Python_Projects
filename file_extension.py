3e4666c7409c15f1d119156ad8dbfab4937c17cf
BrettJ-1 <167814883+BrettJ-1@users.noreply.github.com>
2024-06-27 08:19:30 -0700

    Update

--- file_extension.py
+++ file_extension.py
@@ -5,7 +5,7 @@ conn = sqlite3.connect('file_extension.db')
 with conn:
     cur = conn.cursor()
     cur.execute("CREATE TABLE IF NOT EXISTS tbl_extension( \ 
-        ID INTEGER PRIMARY KEY AUTOINCREMENT, \                   #Created datbase with integer and string
+        ID INTEGER PRIMARY KEY AUTOINCREMENT, \                 #Created datbase with integer and string
         col_filename TEXT,)")
     conn.commit()
 conn.close()
@@ -14,14 +14,14 @@ conn = sqlite3.connect('file_extension.db')
 
 with conn:
     cur = conn.cursor()
-    cur.execute("INSERT INTO tbl_extension(information.docx, Hello.txt, myImage.png, myMovie.mpg, World.txt, data.pdf, myPhoto.jpg) VALUES (?, ?, ?, ?, ?, ?, ?)"  #Inserted values into tbl_extension
+    cur.execute("INSERT INTO col_filename(information.docx, Hello.txt, myImage.png, myMovie.mpg, World.txt, data.pdf, myPhoto.jpg) VALUES (?, ?, ?, ?, ?, ?, ?)"  #Inserted values into col_filename
 conn.close()
 
 conn = sqlite3.connect('file_extension.db')
 
 with conn:
     cur = conn.cursor()
-    cur.execute("SELECT information.docx, Hello.txt, myImage.png, myMovie.mpg, World.txt, data.pdf, myPhoto.jpg FROM tbl_extension WHERE TEXT = '.txt'") #Selecting data with the filename extension of .txts
+    cur.execute("SELECT information.docx, Hello.txt, myImage.png, myMovie.mpg, World.txt, data.pdf, myPhoto.jpg FROM col_filename WHERE TEXT = '.txt'") #Selecting data with the filename extension of .txts
     varPerson = cur.fetchall()
     for item in varPerson:
         msg = "First: ()\nSecond: ()".format(item[0],item[1])
