cac7d035e140515031b5719fa1dd76a95839a873
BrettJ-1 <167814883+BrettJ-1@users.noreply.github.com>
2024-06-27 11:18:10 -0700

    Update

--- file_extension.py
+++ file_extension.py
@@ -1,28 +1,21 @@
 import sqlite3
-
-conn = sqlite3.connect('file_extension.db')
-
+conn = sqlite3.connect('test.db')
 with conn:
     cur = conn.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS tbl_extension( \ 
-        ID INTEGER PRIMARY KEY AUTOINCREMENT, \                 #Created datbase with integer and string
-        col_filename TEXT,)")
+    cur.execute("CREATE TABLE IF NOT EXISTS tbl_extension(ID INTEGER PRIMARY KEY AUTOINCREMENT,\
+                col_filename TEXT)")
     conn.commit()
+
+conn = sqlite3.connect('extension.db')
+
+fileList = ('information.docx', 'Hello.txt', 'myImage.ong',\
+            'myMovie.mpg', 'World. txt', 'data.pdf', 'myPhoto.jpg')
+
+for x in fileList:
+    if x.endswith('.txt'):
+        with conn:
+            cur = conn.cursor()
+            cur.execute("INSERT INTO tbl_extension(col_filename) VALUES (?)", (x,))
+            print(x)
+
 conn.close()
-
-conn = sqlite3.connect('file_extension.db')
-
-with conn:
-    cur = conn.cursor()
-    cur.execute("INSERT INTO col_filename(information.docx, Hello.txt, myImage.png, myMovie.mpg, World.txt, data.pdf, myPhoto.jpg) VALUES (?, ?, ?, ?, ?, ?, ?)"  #Inserted values into col_filename
-conn.close()
-
-conn = sqlite3.connect('file_extension.db')
-
-with conn:
-    cur = conn.cursor()
-    cur.execute("SELECT information.docx, Hello.txt, myImage.png, myMovie.mpg, World.txt, data.pdf, myPhoto.jpg FROM col_filename WHERE TEXT = '.txt'") #Selecting data with the filename extension of .txts
-    varPerson = cur.fetchall()
-    for item in varPerson:
-        msg = "First: ()\nSecond: ()".format(item[0],item[1])
-    print(msg)
