ac4192f3082facf43370cbec15af1cbec4b2e365
BrettJ-1 <167814883+BrettJ-1@users.noreply.github.com>
2024-07-08 17:57:24 -0700

    Added Comments

--- DjangoUniversity/Classes/campusApp/admin.py
+++ DjangoUniversity/Classes/campusApp/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import UniversityCampus
+from .models import UniversityCampus #importing UniversityCampus Module
 
 # Register your models here.
+
+admin.site.register(UniversityCampus) #Creating a UniversityCampus model
