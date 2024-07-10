e90dd1dd5fbb44987ff973a1635a0995d9de8925
BrettJ-1 <167814883+BrettJ-1@users.noreply.github.com>
2024-07-10 11:05:01 -0700

    Checkbook Project

--- DjangoUniversity/Classes/campusApp/models.py
+++ DjangoUniversity/Classes/campusApp/models.py
@@ -3,9 +3,9 @@ from django.db import models
 # Create your models here.
 
 class UniversityCampus(models.Model):
-    campus_name = models.CharField(max_length=60, default="", blank=True, null=False)
-    campus_id = models.IntegerField(default="", blank=True, null=False)
-    state = models.CharField(max_length=2, default="", blank="True", null="False")
+    campus_name = models.CharField(max_length=60, default="", blank=True, null=False) #Creating a Character Field
+    campus_id = models.IntegerField(default="", blank=True, null=False) #Creating an Integer Field
+    state = models.CharField(max_length=2, default="", blank="True", null="False") #Creating a Character Field
 
 object = models.Manager()
 
@@ -14,4 +14,4 @@ def __str__(self):
     return display_course.format(self)
 
 class Meta:
-    verbose_name_plural = "University Campus"
+    verbose_name_plural = "University Campus" #Prevents Plural Names
--- DjangoUniversity/Classes/campusApp/settings.py
+++ DjangoUniversity/Classes/campusApp/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+INSTALLED_APPS = [ #Shows which apps are installed in django project
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
